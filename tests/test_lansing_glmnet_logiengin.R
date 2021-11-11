f7d40adf51d24654688e59953201a19b55b11995
YunyiShen <yshen99@wisc.edu>
2021-11-10 19:40:00 -0600

    things working

--- tests/test_lansing_glmnet_logiengin.R
+++ tests/test_lansing_glmnet_logiengin.R
@@ -7,15 +7,15 @@ source("./R/glmnet_logiengine.R")
 lansing <- unique.ppp( spatstat.data::lansing)
 unique_marks <- levels(marks(lansing))
 
-radii1 <- matrix(1e-1,length(unique_marks),length(unique_marks))
-radii2 <- matrix(5e-2,length(unique_marks),length(unique_marks))
-interaction1 <- MultiStrauss(radii1)
-interaction2 <- MultiStrauss(radii2)
-interaction <- Hybrid(interaction1, interaction2)
+
+radii3 <- matrix(5e-3,length(unique_marks),length(unique_marks))
+interaction3 <- MultiStrauss(radii3)
+interaction <- Hybrid(interaction3)
 
 n_p <- 2250
-n_d <- 1000
+n_d <- 3000
 
+set.seed(42)
 dummy <- data.frame(x = runif(n_d, 0,1), 
             y = runif(n_d, 0,1), 
             mark = factor( sample(unique_marks, n_d, T), 
@@ -26,17 +26,14 @@ dummy_pp <- ppp(x = dummy[,1],y = dummy[,2], marks = dummy[,3] ,
 
 Q <- quadscheme.logi(lansing, dummy_pp)
 
-env_p <- data.frame(e1 = rnorm(n_p), e2 = rnorm(n_p))
-env_d <- data.frame(e1 = rnorm(n_d), e2 = rnorm(n_d))
-
-trend <- ~marks + e1:marks+e2:marks -1
-penalty.factor <- c(rep(0,6) ,rep(1, 12))
+trend <- ~marks - 1
+penalty.factor <- c(rep(0,6) )
 lambda <- NULL
-covariates <- rbind(env_p,env_d)
+
 res_obj <- glmnet.logi.engine(Q, trend, interaction = interaction, 
             penalty.factor = penalty.factor, lambda = lambda, 
-            covariates = covariates)
+            covariates = NULL, cv = F)
 
 res_obj_logi <- ppm(Q, trend = trend, 
                     interaction = interaction, 
-                    covariates  = covariates)
+                    covariates  = NULL)
--- tests/test_simu_glmnet_logiengin.R
+++ tests/test_simu_glmnet_logiengin.R
@@ -6,45 +6,45 @@ source("./R/glmnet_logiengine.R")
 set.seed(42)
 n_spp <- 5
 spp_names <- paste0("spp_",1:n_spp)
-radii <- matrix(5e-2,n_spp,n_spp)
+radii <- matrix(5e-3,n_spp,n_spp)
 log_gammas <- rsparsematrix(n_spp,n_spp, 
             density = 0.2,
             rand.x = function(n){runif(n,-3,-1)})
 log_gammas <- (log_gammas + t(log_gammas))/2
 rownames(log_gammas) <- colnames(log_gammas) <- spp_names
 
-beta <- exp(rnorm(n_spp, 4.5,1))
+beta <- exp(rnorm(n_spp, 6,0.5))
 
 pp <- rmh(model = rmhmodel(cif = "straussm", 
                             par = list(beta = beta, 
                             gamma = exp(as.matrix(log_gammas)),
                             radii = radii),
                             trend = NULL,
-                            w = owin(xrange = c(-1,1),
-                                yrange = c(-1,1)),
+                            w = owin(xrange = c(0,1),
+                                yrange = c(0,1)),
                             types = spp_names))
 pp
-n_d <- 1000
-dummy <- data.frame(x = runif(n_d, -1,1), 
-            y = runif(n_d, -1,1), 
+n_d <- 2000
+dummy <- data.frame(x = runif(n_d, 0,1), 
+            y = runif(n_d, 0,1), 
             mark = factor( sample(spp_names, n_d, T), 
             levels = spp_names))
 
 dummy_pp <- ppp(x = dummy[,1],y = dummy[,2], marks = dummy[,3] ,
-             window = owin(xrange = c(-1,1),
-                           yrange = c(-1,1)))
+             window = owin(xrange = c(0,1),
+                           yrange = c(0,1)))
 
 Q <- quadscheme.logi(pp, dummy_pp)
 
 
 trend <- ~marks  -1
-interaction <- MultiStrauss(radii)
+interaction <- MultiStrauss(1*radii)
 
 penalty.factor <- c(rep(0,n_spp))
 lambda <- NULL
 res_obj <- glmnet.logi.engine(Q, trend, interaction = interaction, 
             penalty.factor = penalty.factor, lambda = lambda, 
-            covariates = NULL, cv = FALSE)
+            covariates = NULL, cv = T)
 
 res_obj_logi <- ppm(Q, trend = trend, 
                     interaction = interaction, 
